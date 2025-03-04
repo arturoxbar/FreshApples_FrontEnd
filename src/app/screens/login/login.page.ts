59f899da9e916015a2855b915df86e57252900ff
arturoxbar <101169728+arturoxbar@users.noreply.github.com>
2025-03-03 20:14:58 -0400

    connected to backend

--- src/app/screens/login/login.page.ts
+++ src/app/screens/login/login.page.ts
@@ -1,4 +1,7 @@
+import { ParseSourceFile } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+import { UserService, ExistingUserInterface } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +9,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
   standalone: false
 })
+
+
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  existingUser: ExistingUserInterface = {
+    userInfo: '',
+    password: ''
+  };
+
+  loading = false;
+  validationError = '';
+
+  constructor(
+    private alert: AlertController,
+    private navCtrl: NavController,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+  }
+
+  private validateForm() {
+    if (!this.existingUser.password || !this.existingUser.userInfo) {
+      this.validationError = 'All the fields are required';
+      this.showAlert('Error', this.validationError);
+      return false;
+    }
+    return true;
+  }
+
+  private async showAlert(header: string, message: string) {
+    const alert = await this.alert.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  private resetForm() {
+    this.existingUser = {
+      userInfo: '',
+      password: ''
+    }
+  }
+
+  async login() {
+    if (!this.validateForm()) {
+      return;
+    }
+
+    this.loading = true;
+    this.validationError = '';
+    try {
+      console.log(this.existingUser)
+      await this.userService.loginUser(this.existingUser).subscribe({
+        next: async (response) => {
+          await this.showAlert('Success', 'User Successfully log in')
+          this.resetForm()
+          console.log(response)
+        },
+        error: async (error) => {
+          console.log("error service", error)
+          const errorMessage = error.error?.message || 'Unknow error';
+          await this.showAlert('Error', errorMessage);
+        }
+      })
+    } catch (error) {
+      await this.showAlert('Error', 'Unknow error2');
+    } finally {
+      this.loading = false
+    }
   }
 
 }
