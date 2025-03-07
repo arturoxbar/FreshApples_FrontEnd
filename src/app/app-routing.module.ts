9ad7af9f67b0f51041dcdb909fef203974250beb
arturoxbar <101169728+arturoxbar@users.noreply.github.com>
2025-03-07 09:09:59 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./screens/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'verification',
+    loadChildren: () => import('./screens/verification/verification.module').then( m => m.VerificationPageModule)
   }
 
 
