588fb4ad97dfe83309fa7082c90901e50c3b8cf9
arturoxbar <101169728+arturoxbar@users.noreply.github.com>
2025-03-07 09:08:45 -0400

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,45 +1,53 @@
-  import { Injectable } from '@angular/core';
-  import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-  import { Observable, catchError, throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
-  export interface UserInterface {
-    username: string;
-    email: string;
-    password: string;
+export interface UserInterface {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface ExistingUserInterface {
+  userInfo: string;
+  password: string;
+}
+
+const ENDPOINTS = {
+  CREATE: "/api/v1/users/signup",
+  LOGIN: "/api/v1/users/login",
+  VALIDATE: "/api/v1/users/user"
+}
+
+@Injectable()
+export class UserService {
+  private baseUrl = 'http://localhost:7338';
+
+  constructor(private http: HttpClient) { }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('Error en la petición:', error);
+    return throwError(() => error);
   }
 
-  export interface ExistingUserInterface {
-    userInfo: string;
-    password: string;
+  createUser(newUser: UserInterface) {
+    const url = `${this.baseUrl}${ENDPOINTS.CREATE}`;
+    return this.http.post(url, newUser).pipe(
+      catchError(this.handleError)
+    );
   }
 
-  const ENDPOINTS = {
-    CREATE: "/api/v1/users/signup",
-    LOGIN: "/api/v1/users/login",
+  loginUser(existingUser: ExistingUserInterface) {
+    const url = `${this.baseUrl}${ENDPOINTS.LOGIN}`;
+    return this.http.post(url, existingUser).pipe(
+      catchError(this.handleError)
+    );
   }
 
-  @Injectable()
-  export class UserService {
-    private baseUrl = 'http://localhost:7338';
-
-    constructor(private http: HttpClient) { }
-
-    private handleError(error: HttpErrorResponse) {
-      console.error('Error en la petición:', error);
-      return throwError(() => error);
-    }
-
-    createUser(newUser: UserInterface) {
-      const url = `${this.baseUrl}${ENDPOINTS.CREATE}`;
-      return this.http.post(url, newUser).pipe(
-        catchError(this.handleError)
-      );
-    }
-
-    loginUser(existingUser: ExistingUserInterface) {
-      const url = `${this.baseUrl}${ENDPOINTS.LOGIN}`;
-      return this.http.post(url, existingUser).pipe(
-        catchError(this.handleError)
-      );
-    }
+  validateUser(userInfo: string, code: string) {
+    const url = `${this.baseUrl}${ENDPOINTS.VALIDATE}${userInfo}/${code}`;
+    return this.http.patch(url, code).pipe(
+      catchError(this.handleError)
+    )
   }
+}
